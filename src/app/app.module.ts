faac947e6c2e247cac7c371def6ccf6c4b05fac1
DEEP ADHIYA <adhiyadeep@gmail.com>
2019-01-07 19:38:44 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {GooglePlus } from '@ionic-native/google-plus';
+import { AngularFireModule } from 'angularfire2';
+import firebase from 'firebase';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
+
+export const firebaseconfig ={
+    apiKey: "AIzaSyDc4rrIzrgxPd-SGL4TJwPt50-xM5gDcWc",
+    authDomain: "goodaye-customer.firebaseapp.com",
+    databaseURL: "https://goodaye-customer.firebaseio.com",
+    projectId: "goodaye-customer",
+    storageBucket: "goodaye-customer.appspot.com",
+    messagingSenderId: "1055286123790"
+}
+firebase.initializeApp(firebaseconfig);
 
 @NgModule({
   declarations: [
@@ -14,7 +28,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),AngularFireModule.initializeApp(firebaseconfig),AngularFireAuthModule
+    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -23,8 +38,11 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    SplashScreen,Facebook,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},GooglePlus
   ]
 })
-export class AppModule {}
+export class AppModule {
+  
+}
+
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [GooglePlus]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private googlePlus: GooglePlus,
+    public afAuth: AngularFireAuth,private fb: Facebook) {
 
   }
+
+  login() {
+
+    this.googlePlus.login({})
+     .then(res => {
+      
+        alert(res.displayName);
+        alert(res.familyName);
+        alert(res.userId);
+        alert(res.email);
+
+     })
+     .catch(err => console.error(err));
+
+ }
+
+ fbLogin()
+{
+    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider)
+    .then(res =>{
+      console.log(res);
+    }, err=>{
+      console.log(err);
+    })
+
+   
+
+}
 
 }
